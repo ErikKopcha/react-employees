71fd7ffe9470ef1ea7161dd9a41112ca48f03b5e
Erik Kopcha <erickopcha@gmail.com>
2021-11-28 00:31:23 +0200

    feat: project structure

---
+++ src/components/app/app.js
@@ -0,0 +1,13 @@
+
+import AppInfo from '../app-info/app-info';
+import './app.scss';
+
+function App() {
+    return (
+        <div className="app">
+            <AppInfo />
+        </div>
+    )
+}
+
+export default App;
