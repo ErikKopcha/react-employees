affa70cae6410f04126a97384903747b2955522d
Erik Kopcha <erickopcha@gmail.com>
2021-11-28 21:09:39 +0200

    update: form

--- src/components/employees-add-form/employees-add-form.js
+++ src/components/employees-add-form/employees-add-form.js
@@ -1,22 +1,50 @@
 import { Component } from "react";
 import './employees-add-form.scss';
 
-class EmployeesAddForm  extends Component {
+class EmployeesAddForm extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            name: '',
+            salary: ''
+        };
+    }
+
+    onValueChange(event) {
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    }
+
     render() {
+        const { name, salary } = this.state;
+
         return (
             <div className="app-add-form">
                 <h3>Add new employee</h3>
                 <form
                     className="add-form d-flex">
-                    <input type="text"
+                    <input 
+                        value={name}
+                        onChange={(e) => this.onValueChange(e)}
+                        name="name"
+                        type="text"
                         className="form-control new-post-label"
                         placeholder="Name" />
-                    <input type="number"
+                    <input 
+                        value={salary}
+                        onChange={(e) => this.onValueChange(e)}
+                        name="salary"
+                        type="number"
                         className="form-control new-post-label"
                         placeholder="Salary in $?" />
     
-                    <button type="submit"
-                            className="btn btn-outline-light">Add employee</button>
+                    <button 
+                        type="submit"
+                        className="btn btn-outline-light">
+                        Add employee
+                    </button>
                 </form>
             </div>
         )
